9fe7605454b2ec65982d0c769228d68d8d18d9e3
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-04-12 10:25:16 +0530

    Virtual Coder

--- virtualcoder/pages/index.js
+++ virtualcoder/pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Script from 'next/Script'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import styles1 from '../styles/Home1.module.css'
+import styles2 from '../styles/Home2.module.css'
 
 export default function Home() {
   return (
@@ -15,23 +18,25 @@ export default function Home() {
       {/* <script src='/sc.js' strategy="lazyOnload"> </script> */}
       <nav className={styles.mainNav}>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Hackathons</li>
-          <li>Projects</li>
+          <Link href='/'><a><li>Home</li></a></Link>
+          <Link href='/about'><a><li>About</li></a></Link>
+          <Link href='/hackathons'><a><li>Hackathons</li></a></Link>
+          <Link href='/projects'><a><li>Projects</li></a></Link>
         </ul>
       </nav>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-        Virtual Coder
+        Virtual Coder  
         </h1>
 
         <p className={styles.description}>
         It is designed for children and/or adults who don't have any experience with coding.
         </p>
 
-        <div className="game">
+        {/* <div className="game"> */}
+        {/* <div className={`${styles2.con} ${styles1.con}`}> */}
+        <div className={styles1.con}>
           <div className="gameItem">
             <h1>An Interactive Puzzle Game</h1>
             <p>It teaches programming principles and introduces JavaScript using a block-based programming approach.</p>
