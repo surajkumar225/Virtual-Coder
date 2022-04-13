abf6a54294a0bb05a434ca8f5b7e35c77d275bb9
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-04-13 16:33:55 +0530

    Virtual coder

---
+++ virtualcoder/components/trase.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Trase = () => {
+  return (
+      <>
+      <style jsx global>
+          {`
+            .trase{
+                background: grey;
+            }
+          `}
+      </style>
+    <div className='trase'>
+        <h2>This is Trase</h2>
+    </div>
+    </>
+  )
+}
+
+export default Trase
