378789de1285c4df9567ab84039db5c48b4a73df
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-04-13 17:16:27 +0530

    Virtual coder

--- virtualcoder/pages/index.js
+++ virtualcoder/pages/index.js
@@ -3,18 +3,19 @@ import Image from 'next/image'
 import Script from 'next/Script'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-import styles1 from '../styles/Home1.module.css'
-import styles2 from '../styles/Home2.module.css'
-import Trase from '../components/trase'
+// import styles1 from '../styles/Home1.module.css'
+// import styles2 from '../styles/Home2.module.css'
+// import Trase from '../components/trase'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Trase/>
+      {/* <Trase/> */}
       <style jsx>
         {`
-        .myspan{
-          color: green;
+         .myimg{
+          border-radius: 10px;
+          margin-top: 30px;
         }
         `}
       </style>
@@ -36,16 +37,18 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-        <span className='myspan trase'> Virtual Coder </span>
+        <span className='myspan'> Virtual Coder </span>
         </h1>
+
+      <img className={styles.myimg} src="/home.jpg" width={537} height={358} />
 
         <p className={styles.description}>
         It is designed for children and/or adults who don't have any experience with coding.
         </p>
 
-        {/* <div className="game"> */}
+        <div className="game">
         {/* <div className={`${styles2.con} ${styles1.con}`}> */}
-        <div className={styles1.con}>
+        {/* <div className={styles1.con}> */}
           <div className="gameItem">
             <h1>An Interactive Puzzle Game</h1>
             <p>It teaches programming principles and introduces JavaScript using a block-based programming approach.</p>
@@ -60,48 +63,11 @@ export default function Home() {
           </div>
         </div>
 
-        {/* <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div> */}
+       
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+       
       </footer>
     </div>
   )
--- virtualcoder/public/sc.js
+++
@@ -1,2 +0,0 @@
-console.log("Hey Trinity")
-alert("Hey")
